71fa529dabb09273ee1e84c8328bca37f78ea81b
OjeshManandhar <ozes.manandhar@gmail.com>
2021-10-09 16:48:32 +0545

    Init web3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+// packages
+import Web3 from 'web3';
 
 // components
 import Test from 'components/Test';
 
 function App() {
-  return <Test str='DeadSkull' />;
+  const [loading, setLoading] = useState(true);
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    async function init() {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+      } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+      } else {
+        alert('Install MetaMask');
+        return;
+      }
+
+      setLoading(false);
+
+      const accounts = await window.web3.eth.getAccounts();
+      console.log('accounts:', accounts);
+      setAccount(accounts[0]);
+    }
+
+    init();
+  }, []);
+
+  return loading ? <h1>Loading</h1> : <Test str={account || ''} />;
 }
 
 export default App;
