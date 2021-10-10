26287978b4398175f60372738e8c4eca5812c2b7
OjeshManandhar <ozes.manandhar@gmail.com>
2021-10-10 13:34:54 +0545

    Get current account

--- src/App.js
+++ src/App.js
@@ -7,32 +7,55 @@ import Web3 from 'web3';
 import Test from 'components/Test';
 
 function App() {
+  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
   const [account, setAccount] = useState(null);
 
   useEffect(() => {
     async function init() {
+      let web3;
+
+      // Select provider
+
       if (window.ethereum) {
-        window.web3 = new Web3(window.ethereum);
-        await window.ethereum.enable();
-      } else if (window.web3) {
-        window.web3 = new Web3(window.web3.currentProvider);
+        console.log('Using ethereum');
+
+        web3 = new Web3(window.ethereum);
+
+        // Add event listeners
+        window.ethereum.on('accountsChanged', accounts => {
+          console.log('accounts on change:', accounts);
+          setAccount(accounts[0]);
+        });
+      } else if (Web3.givenProvider) {
+        console.log('Using web3');
+        web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
       } else {
         alert('Install MetaMask');
+        setError(true);
         return;
       }
 
+      setError(false);
       setLoading(false);
 
-      const accounts = await window.web3.eth.getAccounts();
+      const accounts = await web3.eth.getAccounts();
       console.log('accounts:', accounts);
       setAccount(accounts[0]);
+
+      return web3;
     }
 
     init();
   }, []);
 
-  return loading ? <h1>Loading</h1> : <Test str={account || ''} />;
+  return error ? (
+    <h1>Error</h1>
+  ) : loading ? (
+    <h1>Loading</h1>
+  ) : (
+    <Test str={account || ''} />
+  );
 }
 
 export default App;
