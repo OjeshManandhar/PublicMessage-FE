ac749412e5e27c30adb6c1681cc088e3bf8fc73f
OjeshManandhar <ozes.manandhar@gmail.com>
2021-10-11 14:32:51 +0545

    Remove Test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
-// packages
-// import Web3 from 'web3';
-
 // components
-import Test from 'components/Test';
 import MessagesArea from 'components/MessagesArea';
 import FullScreenCenter from 'components/FullScreenCenter';
-
-// ABI
-// import PublicMessageABI from 'contract_abis/PublicMessages';
 
 // utils
 import web3js from 'utils/Web3js';
@@ -18,80 +11,6 @@ function App() {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
   const [account, setAccount] = useState(null);
-  // const [publicMessage, setPublicMessage] = useState(null);
-
-  // useEffect(() => {
-  //   async function init() {
-  //     let web3;
-
-  //     // Select provider
-  //     if (window.ethereum) {
-  //       console.log('Using ethereum');
-
-  //       web3 = new Web3(window.ethereum);
-
-  //       // Add event listeners
-  //       window.ethereum.on('accountsChanged', accounts => {
-  //         console.log('accounts on change:', accounts);
-  //         setAccount(accounts[0]);
-  //       });
-  //     } else if (Web3.givenProvider) {
-  //       console.log('Using web3');
-  //       web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
-  //     } else {
-  //       alert('Install MetaMask');
-  //       setError(true);
-  //       return;
-  //     }
-
-  //     setError(false);
-  //     setLoading(false);
-
-  //     // Get account
-  //     const accounts = await web3.eth.getAccounts();
-  //     console.log('accounts:', accounts);
-  //     setAccount(accounts[0]);
-
-  //     /*
-  //     // Load contract
-  //     const networkId = await web3.eth.net.getId();
-
-  //     const PublicMessage = PublicMessageABI.networks[networkId];
-  //     if (PublicMessage) {
-  //       const pubMsg = new web3.eth.Contract(
-  //         PublicMessageABI.abi,
-  //         PublicMessage.address
-  //       );
-
-  //       console.log('pubMsg:', pubMsg);
-
-  //       console.log(
-  //         'handlePrice:',
-  //         await pubMsg.methods.getHandlePrice().call()
-  //       );
-
-  //       // await pubMsg.methods.setHandle('Bob').send({
-  //       //   from: accounts[0],
-  //       //   value: '1000000000000000'
-  //       // });
-
-  //       console.log('handle:', await pubMsg.methods.getHandle().call());
-
-  //       console.log(
-  //         'contract balance:',
-  //         web3.utils.fromWei(
-  //           await web3.eth.getBalance(PublicMessage.address),
-  //           'ether'
-  //         )
-  //       );
-  //     }
-  //     */
-
-  //     return web3;
-  //   }
-
-  //   init();
-  // }, []);
 
   const handleAccountChange = useCallback(acc => {
     setAccount(acc);
@@ -116,14 +35,11 @@ function App() {
   }, [handleAccountChange]);
 
   return error ? (
-    <FullScreenCenter>Error</FullScreenCenter>
+    <FullScreenCenter>{error}</FullScreenCenter>
   ) : loading ? (
     <FullScreenCenter>Loading</FullScreenCenter>
   ) : (
-    <>
-      <Test str={account || ''} />
-      <MessagesArea />
-    </>
+    <MessagesArea account={account} />
   );
 }
 
